d70106dbfc5e7713a60d74ed5d853266354e3ef8
javierlnc <javierlnc@icloud.com>
2024-03-04 08:59:01 -0500

    Se crea modal de ayuda y se mejora el del login

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  constructor(private modalService: NgbModal) {
+  }
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
 }

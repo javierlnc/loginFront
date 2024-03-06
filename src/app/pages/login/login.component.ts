3fdf5b27f2aec481df73dbbf11313e486ec312c7
javierlnc <javierlnc@icloud.com>
2024-03-05 21:57:46 -0500

    Se completa servicio y se empieza handle error

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -11,35 +11,44 @@ import { LoginRequest } from 'src/app/services/auth/loginRequest';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginError: string = '';
+  showModal = false;
   loginForm = new FormGroup({
     usuario: new FormControl('', [Validators.required]),
     contraseña: new FormControl('', Validators.required),
   });
-  constructor(private modalService: NgbModal, private router:Router, private loginServies: LoginService) {
+  constructor(private modalService: NgbModal, private router: Router, private loginService: LoginService) {
   }
   public open(modal: any): void {
     this.modalService.open(modal);
   }
   onSubmit() {
-    if(this.loginForm.valid){
-      this.loginServies.login(this.loginForm.value as LoginRequest).subscribe({
-        next(UserData) {
+    if (this.loginForm.valid) {
+      this.loginService.login(this.loginForm.value as LoginRequest).subscribe({
+        next: (UserData) => {
           console.log(UserData);
+
         },
-        error(err) {
+        error: (err) => {
+          this.showModal = true;
           console.error("ha ocurrido un error: " + err);
+          this.loginError = err;
+
+          console.log(this.loginError);
+          console.log(this.showModal)
+          this.loginForm.reset();
+          this.loginForm.markAllAsTouched();
         },
-        complete() {
+        complete: () => {
           console.log("listo");
+          this.loginForm.reset();
+          this.router.navigateByUrl('/home');
         },
       });
-      this.loginForm.reset();
-      this.router.navigateByUrl('/home');
-
-    }else{
-      this.loginForm.markAllAsTouched();
 
     }
   }
+
   get usuario() { return this.loginForm.controls.usuario; }
+  get contraseña() { return this.loginForm.controls.contraseña; }
 }

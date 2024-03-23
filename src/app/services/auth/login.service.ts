d3d8da359da02f20f5561a5a47a1a350a8382817
javierlnc <javierlnc@icloud.com>
2024-03-23 12:03:07 -0500

    Se modifica cursor sobre a

--- src/app/services/auth/login.service.ts
+++ src/app/services/auth/login.service.ts
@@ -1,28 +1,17 @@
 import { Injectable } from '@angular/core';
 import { LoginRequest } from './loginRequest';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor( private httpClient : HttpClient) { }
 
-  login(credentials: LoginRequest): Observable<any> {
-    return this.httpClient.get('https://jsonplaceholder.typicode.com/u sers').pipe(
-      catchError(this.handleError)
-    );
+  login(credentials: LoginRequest):Observable<any>{
+    return this.httpClient.get('https://jsonplaceholder.typicode.com/users')
 
-  }
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      console.error('An error occurred:', error.error);
-    } else {
-      console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
-    };
-    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }

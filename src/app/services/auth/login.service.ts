554c2926db7a8f282868d453ee524f9432dc95c2
javierlnc <javierlnc@icloud.com>
2024-03-26 22:21:21 -0500

    Se agrega el estado de login y la lista del navbar

--- src/app/services/auth/login.service.ts
+++ src/app/services/auth/login.service.ts
@@ -1,30 +1,37 @@
 import { Injectable } from '@angular/core';
 import { LoginRequest } from './loginRequest';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, catchError, throwError, BehaviorSubject, tap } from 'rxjs';
 import { Users } from './users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  currentUserLoginOn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-  constructor( private httpClient : HttpClient) { }
 
-  login(credentials: LoginRequest):Observable<Users>{
-    return this.httpClient.get<Users>('https://gorest.co.i/public/v2/users/6805291')
-    .pipe(
-      catchError(this.handleError)
-    );
+  constructor(private httpClient: HttpClient) { }
+
+  login(credentials: LoginRequest): Observable<Users> {
+    return this.httpClient.get<Users>('https://gorest.co.in/public/v2/users/2139332')
+      .pipe(
+        tap((userData: Users) => { this.currentUserLoginOn.next(true) }),
+        catchError(this.handleError)
+      );
+
 
   }
-  private handleError(error: HttpErrorResponse){
-    if(error.status === 0){
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
       console.error('An error occurred:', error.error);
-    }else{
+    } else {
       console.error(
         `Backend returned code ${error.status}, body was: `, error.error);
     }
     return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+  get userLoginOn(): Observable<boolean> {
+    return this.currentUserLoginOn.asObservable();
   }
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { LoginService } from 'src/app/services/auth/login.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  constructor(private modalService: NgbModal) {
+export class HeaderComponent implements OnInit {
+  userLoginOn: boolean = false;
+  constructor(private modalService: NgbModal, private loginService: LoginService) {
+  }
+  ngOnInit(): void {
+    this.loginService.currentUserLoginOn.subscribe(
+      {
+        next: (userLoginOn) => {
+          this.userLoginOn = userLoginOn;
+        }
+      }
+    )
   }
   public open(modal: any): void {
     this.modalService.open(modal);
